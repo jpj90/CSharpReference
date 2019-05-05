9b453794dd8a3e79bb3c76ae44e3bfc171d85434
James Janse <jpauljanse@gmail.com>
2019-05-05 21:08:45 +0200

    added exercises 2 and 3

--- CSharpExamReference/Chapter1/Skill1.1/TaskParallelLibrary.cs
+++ CSharpExamReference/Chapter1/Skill1.1/TaskParallelLibrary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,9 +9,12 @@ namespace CSharpExamReference.Chapter1.Skill1._1
   {
     public static void SubMain()
     {
-      FirstExercise();
+      //FirstExercise();
+      //SecondExcercise();
+      ThirdExercise();
       Console.Read();
     }
+    #region exercise 1
 
     static void FirstExercise()
     {
@@ -44,5 +48,50 @@ namespace CSharpExamReference.Chapter1.Skill1._1
       Thread.Sleep(1000);
       Console.WriteLine("Task 3 is ending");
     }
+    #endregion
+
+    #region exercise 2 and 3
+
+    static void SecondExcercise()
+    {
+      var items = Enumerable.Range(0, 100);
+      /*
+       * The ForEach method accepts two parameters: the first
+       * is an IEnumerable collection and the second a delegate
+       * to be performed on each one of them. Note that this 
+       * method has a lot more options that the simple Invoke 
+       * method. Notice that the tasks are not completed in 
+       * the order that they were started!
+       */
+      Parallel.ForEach(items, item => WorkOnItem(item));
+    }
+
+    static void ThirdExercise()
+    {
+      var items = Enumerable.Range(0, 10).ToArray();
+      /*
+       * The Parallel.For method is passed a counter starting at
+       * an arbitrary position and running until the second parameter
+       * is reached. The third argument is a delegate that is passed
+       * a variable representing the iterator, starting at the value
+       * of the first argument and running until the value of the second
+       * argument. After executing all its cycles, the Parallel.For 
+       * method returns. 
+       */
+      Parallel.For(3, items.Length, i =>
+      {
+        WorkOnItem(items[i]);
+      });
+
+      Console.WriteLine("Finished processsing Parallel.For()");
+    }
+
+    static void WorkOnItem(object item)
+    {
+      Console.WriteLine("Started working on: " + item);
+      Thread.Sleep(300);
+      Console.WriteLine("Finished working on: " + item);
+    }
+    #endregion
   }
 }
